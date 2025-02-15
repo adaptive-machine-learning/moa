5ee5f9fd27230ff4f77006a7d222c4f648a77648
nuwangunasekara <ng198002@gmail.com>
2025-02-15 22:00:58 +0100

    Add copyright and author information

--- moa/src/main/java/moa/classifiers/trees/StreamingGradientTreePredictor.java
+++ moa/src/main/java/moa/classifiers/trees/StreamingGradientTreePredictor.java
@@ -1,3 +1,22 @@
+/*
+ *    StreamingGradientTreePredictor.java
+ *    Copyright (C) 2025 University of Waikato, Hamilton, New Zealand
+ *    @author Nuwan Gunasekara (ng198002@gmail.com)
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package moa.classifiers.trees;
 
 import com.github.javacliparser.FloatOption;
@@ -11,7 +30,14 @@ import moa.classifiers.trees.sgtutil.*;
 import moa.core.Measurement;
 
 import java.io.Serializable;
-
+/**
+ *
+ * <p> See details in: <br> Henry Gouk, Bernhard Pfahringer, Eibe Frank. </br>
+ * Stochastic Gradient Trees.
+ * Proceedings of The Eleventh Asian Conference on Machine Learning, 2019.
+ * https://proceedings.mlr.press/v101/gouk19a.html </p>
+ *
+ */
 public class StreamingGradientTreePredictor extends AbstractClassifier implements Serializable, MultiClassClassifier, Regressor {
 
     private static final long serialVersionUID = 1L;
